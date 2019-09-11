7844e2757653c34ed29c6cc84ff298dd9dbeb41f
madsa101 <lundbye.mads@yahoo.no>
2019-09-11 13:17:05 +0200

    Initial commit

---
+++ joptiontull/src/Collections/Main.java
@@ -0,0 +1,12 @@
+package Collections;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int a;
+	    a = Integer.parseInt(JOptionPane.showInputDialog("input first integer"));
+	    System.out.println(a);
+    }
+}
